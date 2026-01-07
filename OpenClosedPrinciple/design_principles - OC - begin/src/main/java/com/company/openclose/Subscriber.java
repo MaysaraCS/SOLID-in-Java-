a4cfbdf902296c263b2859318de6d5a7007677a6
Maysara <maysaracs1001@gmail.com>
2026-01-07 13:01:14 +0300

    inheritance used

--- OpenClosedPrinciple/design_principles - OC - begin/src/main/java/com/company/openclose/Subscriber.java
+++ OpenClosedPrinciple/design_principles - OC - begin/src/main/java/com/company/openclose/Subscriber.java
@@ -1,6 +1,49 @@
 package com.company.openclose;
 
+// base class
+// closed for modification
+public abstract class Subscriber {
 
-public class Subscriber {
+    protected Long subscriberId;
 
+    protected String address;
+
+    protected Long phoneNumber;
+
+    protected int baseRate;
+
+    public Long getSubscriberId() {
+        return subscriberId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public int getBaseRate() {
+        return baseRate;
+    }
+
+    public void setSubscriberId(Long subscriberId) {
+        this.subscriberId = subscriberId;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setBaseRate(int baseRate) {
+        this.baseRate = baseRate;
+    }
+
+    // extension
+    public abstract double calculateBill();
 }
