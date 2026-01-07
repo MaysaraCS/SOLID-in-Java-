339e462dd1137bab3b4cc084fe6b2da873e7d953
Maysara <maysaracs1001@gmail.com>
2026-01-07 12:38:16 +0300

    used 2 classes for 2 uses

--- SingleResponsibilityPrinciple/design_principles - SRP - hands on begin/src/main/java/com/company/singleresp/UserPersistenceService.java
+++ SingleResponsibilityPrinciple/design_principles - SRP - hands on begin/src/main/java/com/company/singleresp/UserPersistenceService.java
@@ -2,5 +2,8 @@ package com.company.singleresp;
 
 public class UserPersistenceService {
 
-	
+    private Store store = new Store();
+	public void saveUser(User user) {
+        store.store(user);
+    }
 }
