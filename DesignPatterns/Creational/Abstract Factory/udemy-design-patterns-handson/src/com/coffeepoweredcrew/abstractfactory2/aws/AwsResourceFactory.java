2305af21fb491b18ce5412778d5daf9ad6f51300
Maysara <maysaracs1001@gmail.com>
2026-01-09 16:38:13 +0300

    abstract factory done

--- DesignPatterns/Creational/Abstract Factory/udemy-design-patterns-handson/src/com/coffeepoweredcrew/abstractfactory2/aws/AwsResourceFactory.java
+++ DesignPatterns/Creational/Abstract Factory/udemy-design-patterns-handson/src/com/coffeepoweredcrew/abstractfactory2/aws/AwsResourceFactory.java
@@ -6,5 +6,14 @@ import com.coffeepoweredcrew.abstractfactory2.Storage;
 //Factory implementation for Google cloud platform resources
 public class AwsResourceFactory implements ResourceFactory {
 
+    @Override
+    public Instance createInstance(Capacity capacity) {
+        return new Ec2Instance(capacity);
+    }
+
+    @Override
+    public Storage createStorage(int capMib) {
+        return new S3Storage(capMib);
+    }
 
 }
