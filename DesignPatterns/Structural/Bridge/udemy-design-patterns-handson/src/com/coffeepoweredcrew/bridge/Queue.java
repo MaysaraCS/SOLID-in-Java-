c09483eef0119ae9e94a31d6dd281e22c91d9737
Maysara <maysaracs1001@gmail.com>
2026-01-21 16:30:43 +0300

    bridge done

--- DesignPatterns/Structural/Bridge/udemy-design-patterns-handson/src/com/coffeepoweredcrew/bridge/Queue.java
+++ DesignPatterns/Structural/Bridge/udemy-design-patterns-handson/src/com/coffeepoweredcrew/bridge/Queue.java
@@ -1,7 +1,20 @@
 package com.coffeepoweredcrew.bridge;
 
 //A refined abstraction.
-public class Queue<T> {
-	
-	
+public class Queue<T> implements FifoCollection{
+
+    private LinkedList<T> list;
+
+    public Queue(LinkedList<T> lsit){
+        this.list = lsit;
+    }
+	@Override
+    public void offer(T element){
+        list.addLast(element);
+    }
+
+    @Override
+    public T poll(){
+        return list.removeFirst();
+    }
 }
